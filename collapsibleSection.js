81bd45cbeed87f200f4dcb39e06e503968400ea7
Dave Lockhart <Dave.Lockhart@desire2learn.com>
2014-01-10 13:43:51 -0500

    Initial collapsible section widget.

--- collapsibleSection.js
+++ collapsibleSection.js
@@ -20,36 +20,117 @@
 		module.exports = vui;
 	}
 
+	var collapsedClassName = 'vui-heading-collapsible-collapsed';
+
 	var $ = vui.$;
 
 	$.widget( 'vui.vui_collapsibleSection', {
 
 		_create: function() {
 
-			var $node = $( this.element );
+			var me = this;
 
-			
+			var $elem = this.element;
+
+			var targetId = $elem.attr( 'data-target' );
+			if( targetId === undefined ) {
+				return;
+			}
+
+			var target = document.getElementById( targetId );
+			if( target === null ) {
+				return;
+			}
+
+			this.target = $( target );
+			var targetIsVisible = this.target.is(":visible");
+
+			var headingText = $elem.text();
+			var hideText = 'Hide ' + headingText;
+			var showText = 'Show ' + headingText;
+
+			this.anchor = $( '<a class="foo vui-link" href="javascript:void(0);" aria-controls="' + targetId + '"><span class="vui-offscreen"></span></a>' );
+			$elem.append( this.anchor );
+
+			var evtData = {
+					hideText: hideText,
+					showText: showText,
+					target: this.target
+				};
+
+			$elem
+				.on( 'collapse.vui', evtData, this._handleCollapse )
+				.on( 'expand.vui', evtData, this._handleExpand )
+				.on( 'click', this._handleClick )
+				.trigger(  targetIsVisible ? 'expand.vui': 'collapse.vui' );
 
 		},
 
 		_destroy: function () {
 
-			var $node = $( this.element );
+			var $elem = this.element;
 
-			if( !$node.is( 'a' ) ) {
-				return;
-			}
+			$elem
+				.off( 'collapse.vui', this._handleCollapse )
+				.off( 'epxand.vui', this._handleExpand )
+				/*.off( 'click', this._handleClick )*/
+				.removeClass( collapsedClassName );
 
-			$node.removeAttr( 'role' )
-				.removeAttr( 'aria-disabled' )
-				.removeAttr( 'tabIndex' );
+			this.anchor.remove();
+			this.target.removeAttr( 'aria-hidden' );
+
+		},
+
+		_handleCollapse: function( evt ) {
+
+			$( this )
+				.addClass( collapsedClassName )
+				.find( '.foo' )
+					.attr( 'aria-expanded', false )
+				.find( '.vui-offscreen').text( evt.data.showText );
+
+			evt.data.target.slideUp(
+					function() {
+						$( this )
+							.addClass( 'vui-hidden' )
+							.removeAttr( 'style' )
+							.attr( 'aria-hidden', true );
+					}
+				);
+
+		},
+
+		_handleClick: function( evt ) {
+
+			var isCollapsed = $( this ).is('.vui-heading-collapsible-collapsed');
+			$( this ).trigger( isCollapsed ? 'expand.vui' : 'collapse.vui' );
+			console.log( 'click' );
+
+		},
+
+		_handleExpand: function( evt ) {
+
+			$( this )
+				.removeClass( collapsedClassName )
+				.find( '.foo' )
+					.attr( 'aria-expanded', true )
+				.find( '.vui-offscreen' ).text( evt.data.hideText );
+
+			evt.data.target.slideDown(
+					function() {
+						$( this )
+							.removeClass( 'vui-hidden' )
+							.removeAttr( 'style' )
+							.attr( 'aria-hidden', false );
+					}
+				);
 
 		}
 
 	} );
 
 	vui.addClassInitializer(
-			'vui-collapsibleSection',
+			'vui-heading-collapsible',
 			function( node ) {
 				$( node ).vui_collapsibleSection();
 			}
